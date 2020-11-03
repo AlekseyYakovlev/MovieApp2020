801f9baac4d3a25d552869d1d8a347082f2a3bd7
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2020-11-03 14:34:52 +0300

    Second activity is added. One can now navigate from the main activity to the second activity by clicking on "Hello world".

--- app/src/main/java/ru/spb/yakovlev/androidacademy2020/MainActivity.kt
+++ app/src/main/java/ru/spb/yakovlev/androidacademy2020/MainActivity.kt
@@ -9,5 +9,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.first_activity_text_view)
+
+        textView.setOnClickListener { moveToNextScreen() }
+    }
+
+    private fun moveToNextScreen() {
+        val transmittedString = "String to transmit"
+        val transmittedInt = 12
+        val transmittedBoolean = true
+
+
+        val intent = Intent(this, SecondActivity::class.java).apply {
+            putExtra(SecondActivity.TRANSMITTED_STRING, transmittedString)
+            putExtra(SecondActivity.TRANSMITTED_INT, transmittedInt)
+            putExtra(SecondActivity.TRANSMITTED_BOOLEAN, transmittedBoolean)
+        }
+
+        startActivity(intent)
     }
 }
