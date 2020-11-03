65fe367fa21f7cc4a705cc4089bb2843488ca515
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2020-11-03 14:26:17 +0300

    Layout of first activity is modified for bilingual support in portrait mode, also landscape layout is added.

--- app/src/main/java/ru/spb/yakovlev/androidacademy2020/MainActivity.kt
+++ app/src/main/java/ru/spb/yakovlev/androidacademy2020/MainActivity.kt
@@ -1,7 +1,9 @@
 package ru.spb.yakovlev.androidacademy2020
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
