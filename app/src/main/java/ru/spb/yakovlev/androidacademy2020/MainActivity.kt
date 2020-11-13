4439f6dcbc1276cb97323db8fd955c06ff54392e
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2020-11-14 02:14:08 +0300

    Deleted unused files

--- app/src/main/java/ru/spb/yakovlev/androidacademy2020/MainActivity.kt
+++ app/src/main/java/ru/spb/yakovlev/androidacademy2020/MainActivity.kt
@@ -1,32 +1,5 @@
 package ru.spb.yakovlev.androidacademy2020
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.first_activity_text_view)
-
-        textView.setOnClickListener { moveToNextScreen() }
-    }
-
-    private fun moveToNextScreen() {
-        val transmittedString = "String to transmit"
-        val transmittedInt = 12
-        val transmittedBoolean = true
-
-
-        val intent = Intent(this, SecondActivity::class.java).apply {
-            putExtra(SecondActivity.TRANSMITTED_STRING, transmittedString)
-            putExtra(SecondActivity.TRANSMITTED_INT, transmittedInt)
-            putExtra(SecondActivity.TRANSMITTED_BOOLEAN, transmittedBoolean)
-        }
-
-        startActivity(intent)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.movie_details)
