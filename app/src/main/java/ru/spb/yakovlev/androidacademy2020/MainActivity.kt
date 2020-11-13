0d189a2d39ee61e4bd677319fe4eb68f35fc4eee
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2020-11-14 02:24:26 +0300

    Work shop 1 3 (#1)
    * Layout of first activity is modified for bilingual support in portrait mode, also landscape layout is added.
    
    * Second activity is added.
    One can now navigate from the main activity to the second activity by clicking on "Hello world".
    
    * Deleted unused files

--- app/src/main/java/ru/spb/yakovlev/androidacademy2020/MainActivity.kt
+++ app/src/main/java/ru/spb/yakovlev/androidacademy2020/MainActivity.kt
@@ -1,11 +1,5 @@
 package ru.spb.yakovlev.androidacademy2020
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.movie_details)
