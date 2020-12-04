e95c2a2b5682ec3c4dac59d1a2c8a960a2d8608a
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2020-12-04 17:49:07 +0300

    Base DTOs + dummy repos

--- app/src/main/java/ru/spb/yakovlev/androidacademy2020/model/MovieDetailsData.kt
+++ app/src/main/java/ru/spb/yakovlev/androidacademy2020/model/MovieDetailsData.kt
@@ -1,6 +1,6 @@
 package ru.spb.yakovlev.androidacademy2020.model
 
-data class MovieItemData(
+data class MovieDetailsData (
     val id: Int = 0,
     val title: String = "",
     val tags: String = "",
@@ -8,6 +8,8 @@ data class MovieItemData(
     val pg: String = "6+",
     val rating: Float = 0f,
     val reviewsCount: Int = 0,
-    val poster: String = "",
-    val isLike: Boolean = false
+    val poster2: String = "",
+    val isLike: Boolean = false,
+    val storyLine: String = "",
+    val actorItemsData: List<ActorItemData> = listOf(),
 )
