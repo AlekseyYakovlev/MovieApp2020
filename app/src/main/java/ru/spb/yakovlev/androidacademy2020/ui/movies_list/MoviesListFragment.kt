0d826e684d0cf4d9d3ff491950b93169fa7b017e
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2020-12-04 18:12:32 +0300

    MovieDetailsFragment is added with recyclerview and uses viewmodel to fetch data from

--- app/src/main/java/ru/spb/yakovlev/androidacademy2020/ui/movies_list/MoviesListFragment.kt
+++ app/src/main/java/ru/spb/yakovlev/androidacademy2020/ui/movies_list/MoviesListFragment.kt
@@ -1,53 +1,100 @@
 package ru.spb.yakovlev.androidacademy2020.ui.movies_list
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
+import coil.load
+import coil.metadata
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.flow.collect
+import ru.spb.yakovlev.androidacademy2020.R
 import ru.spb.yakovlev.androidacademy2020.databinding.FragmentMovieItemBinding
 import ru.spb.yakovlev.androidacademy2020.databinding.FragmentMoviesListBinding
+import ru.spb.yakovlev.androidacademy2020.model.DataState
 import ru.spb.yakovlev.androidacademy2020.model.MovieItemData
+import ru.spb.yakovlev.androidacademy2020.utils.viewbindingdelegate.viewBinding
+import ru.spb.yakovlev.androidacademy2020.viewmodels.MoviesListViewModel
 
-class MoviesListFragment : Fragment() {
-    var clickListener: (() -> Unit)? = null
+class MoviesListFragment : Fragment(R.layout.fragment_movies_list) {
+    var clickListener: ((Int) -> Unit)? = null
 
-    val bindVH: (FragmentMovieItemBinding, MovieItemData) -> Unit ={item, data ->
-        with(item){
 
+    private val viewModel: MoviesListViewModel by viewModels()
+    private val vb by viewBinding(FragmentMoviesListBinding::bind)
+
+    private val bindVH: (FragmentMovieItemBinding, MovieItemData) -> Unit = { item, data ->
+        with(item) {
+            ivPoster.load(data.poster) {
+                placeholderMemoryCacheKey(ivPoster.metadata?.memoryCacheKey)
+            }
+            tvPg.text = data.pg
+            ivLike.isChecked = data.isLike
+            ivLike.setOnClickListener { viewModel.handleLike(data.id, !data.isLike) }
+            ratingBar.rating = data.rating
+            tvReview.text =
+                resources.getQuantityString(
+                    R.plurals.movie_details__reviews,
+                    data.reviewsCount,
+                    data.reviewsCount
+                )
+            tvHeader.text = data.title
+            tvTags.text = data.tags
+            tvTiming.text = resources.getString(R.string.minutes, data.duration)
+            root.setOnClickListener { clickListener?.invoke(data.id) }
         }
-
-    }
-
-    private var _binding: FragmentMoviesListBinding? = null
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMoviesListBinding.inflate(inflater, container, false)
-        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.rvMoviesList.apply {
-            GridLayoutManager(context, 2)
-            adapter = MoviesListRVAdapter(bindVH)
-        }
-
-//        binding.card1.root.setOnClickListener {
-//            clickListener?.invoke()
-//        }
+        setupViews()
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onResume() {
+        super.onResume()
+        viewModel.update()
+    }
+
+    private fun setupViews() {
+        val rvAdapter = MoviesListRVAdapter(bindVH)
+        vb.rvMoviesList.apply {
+            layoutManager = GridLayoutManager(
+                context,
+                resources.getInteger(R.integer.movies_list__number_of_columns)
+            )
+            adapter = rvAdapter
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.moviesListState.collect {
+                when (it) {
+                    is DataState.Empty -> {
+                    }
+                    is DataState.Loading -> {
+                        vb.progressBar.visibility = View.VISIBLE
+                        it.progress?.let { progress ->
+                            vb.tvProgressBarText.visibility = View.VISIBLE
+                            vb.tvProgressBarText.text =
+                                resources.getString(R.string.progress, progress)
+                        }
+                    }
+                    is DataState.Success<List<MovieItemData>> -> {
+                        vb.progressBar.visibility = View.GONE
+                        vb.tvProgressBarText.visibility = View.GONE
+                        rvAdapter.updateData(it.data)
+                    }
+                    is DataState.Error -> {
+                        vb.progressBar.visibility = View.GONE
+                        vb.tvProgressBarText.visibility = View.GONE
+                        Snackbar.make(vb.root, it.errorMessage, Snackbar.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
 }
 
---
+++ app/src/main/java/ru/spb/yakovlev/androidacademy2020/viewmodels/MoviesListViewModel.kt
@@ -0,0 +1,62 @@
+package ru.spb.yakovlev.androidacademy2020.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import ru.spb.yakovlev.androidacademy2020.data.MovieListDummyRepo
+import ru.spb.yakovlev.androidacademy2020.model.DataState
+import ru.spb.yakovlev.androidacademy2020.model.MovieItemData
+
+class MoviesListViewModel : ViewModel() {
+    private val _moviesListState = MutableStateFlow<DataState<List<MovieItemData>>>(DataState.Empty)
+    val moviesListState: StateFlow<DataState<List<MovieItemData>>> = _moviesListState
+
+    init {
+        generateDataFlow()
+    }
+
+    fun handleLike(movieId: Int, isLike: Boolean) {
+        MovieListDummyRepo.updateMovieLike(movieId, isLike)
+        update()
+    }
+
+    fun update() {
+        _moviesListState.value = DataState.Success(MovieListDummyRepo.getMovieItemList())
+    }
+
+    /**
+     * Data loading process emulator
+     */
+    private fun generateDataFlow() {
+        viewModelScope.launch {
+            (0..30).forEach {
+                _moviesListState.value = DataState.Loading(it)
+                delay(10L)
+            }
+            _moviesListState.value = DataState.Error("Loading error, restarting loading...")
+            delay(500L)
+            (0..33).forEach {
+                _moviesListState.value = DataState.Loading(it)
+                delay(30L)
+            }
+            delay(500L)
+            _moviesListState.value = DataState.Success(MovieListDummyRepo.movieItemList.take(4))
+            (34..66).forEach {
+                _moviesListState.value = DataState.Loading(it)
+                delay(30L)
+            }
+            delay(500L)
+            _moviesListState.value = DataState.Success(MovieListDummyRepo.movieItemList.take(8))
+            (67..100).forEach {
+                _moviesListState.value = DataState.Loading(it)
+                delay(30L)
+            }
+            delay(500L)
+            _moviesListState.value = DataState.Success(MovieListDummyRepo.getMovieItemList())
+        }
+    }
+
+}
