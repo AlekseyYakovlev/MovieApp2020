c83f7ff4373e472c8fdb5bd56f19b2b56d6b6d6f
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-01-11 07:34:21 +0300

    Retrofit service + Responses

---
+++ app/src/main/java/ru/spb/yakovlev/movieapp2020/data/remote/resp/ConfigurationResponse.kt
@@ -0,0 +1,31 @@
+package ru.spb.yakovlev.movieapp2020.data.remote.resp
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ConfigurationResponse(
+    @SerialName("change_keys")
+    val changeKeys: List<String>,
+    @SerialName("images")
+    val images: Images
+) {
+    @Serializable
+    data class Images(
+        @SerialName("backdrop_sizes")
+        val backdropSizes: List<String>,
+        @SerialName("base_url")
+        val baseUrl: String,
+        @SerialName("logo_sizes")
+        val logoSizes: List<String>,
+        @SerialName("poster_sizes")
+        val posterSizes: List<String>,
+        @SerialName("profile_sizes")
+        val profileSizes: List<String>,
+        @SerialName("secure_base_url")
+        val secureBaseUrl: String,
+        @SerialName("still_sizes")
+        val stillSizes: List<String>
+    )
+}
