2662cab403161455093a00ed317ca869b30fad38
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-01-17 02:07:29 +0300

    class MovieResp renamed to MovieResponse

--- app/src/main/java/ru/spb/yakovlev/movieapp2020/data/remote/resp/MoviesListResponse.kt
+++ app/src/main/java/ru/spb/yakovlev/movieapp2020/data/remote/resp/MoviesListResponse.kt
@@ -11,7 +11,7 @@ data class MoviesListResponse(
     @SerialName("page")
     val page: Int,
     @SerialName("results")
-    val movies: List<MovieResp>,
+    val movies: List<MovieResponse>,
     @SerialName("total_pages")
     val totalPages: Int,
     @SerialName("total_results")
