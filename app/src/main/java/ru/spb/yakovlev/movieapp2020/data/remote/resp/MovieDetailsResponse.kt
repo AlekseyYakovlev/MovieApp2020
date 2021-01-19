d235fba03858a67415483948410f07eacf4311c2
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-01-19 23:53:11 +0300

    Code cleanup + default values for MovieDetailsResponse

--- app/src/main/java/ru/spb/yakovlev/movieapp2020/data/remote/resp/MovieDetailsResponse.kt
+++ app/src/main/java/ru/spb/yakovlev/movieapp2020/data/remote/resp/MovieDetailsResponse.kt
@@ -7,53 +7,53 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MovieDetailsResponse(
     @SerialName("adult")
-    val isAdult: Boolean,
+    val isAdult: Boolean = false,
     @SerialName("backdrop_path")
-    val backdropPath: String,
+    val backdropPath: String = "",
     @SerialName("budget")
-    val budget: Int,
+    val budget: Int = 0,
     @SerialName("genres")
-    val genres: List<Genre>,
+    val genres: List<Genre> = emptyList(),
     @SerialName("homepage")
-    val homepage: String,
+    val homepage: String = "",
     @SerialName("id")
-    val id: Int,
+    val id: Int = 0,
     @SerialName("imdb_id")
-    val imdbId: String,
+    val imdbId: String = "",
     @SerialName("original_language")
-    val originalLanguage: String,
+    val originalLanguage: String = "",
     @SerialName("original_title")
-    val originalTitle: String,
+    val originalTitle: String = "",
     @SerialName("overview")
-    val overview: String,
+    val overview: String = "",
     @SerialName("popularity")
-    val popularity: Double,
+    val popularity: Double = 0.0,
     @SerialName("poster_path")
-    val posterPath: String,
+    val posterPath: String = "",
     @SerialName("release_date")
-    val releaseDate: String,
+    val releaseDate: String = "",
     @SerialName("revenue")
-    val revenue: Int,
+    val revenue: Int = 0,
     @SerialName("runtime")
-    val runtime: Int,
+    val runtime: Int = 0,
     @SerialName("status")
-    val status: String,
+    val status: String = "",
     @SerialName("tagline")
-    val tagline: String,
+    val tagline: String = "",
     @SerialName("title")
-    val title: String,
+    val title: String = "",
     @SerialName("video")
-    val video: Boolean,
+    val video: Boolean = false,
     @SerialName("vote_average")
-    val voteAverage: Double,
+    val voteAverage: Double = 0.0,
     @SerialName("vote_count")
-    val voteCount: Int
+    val voteCount: Int = 0,
 ) {
     @Serializable
     data class Genre(
         @SerialName("id")
-        val id: Int,
+        val id: Int = 0,
         @SerialName("name")
-        val name: String
+        val name: String = "",
     )
 }
