e347ca5939a31e3ccf0a3b46bcf9ad60f57f4d1d
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-01-25 15:42:30 +0300

    fix: ActorEntities are now saved with correct Id

--- app/src/main/java/ru/spb/yakovlev/movieapp2020/data/db/entities/MovieActorXRef.kt
+++ app/src/main/java/ru/spb/yakovlev/movieapp2020/data/db/entities/MovieActorXRef.kt
@@ -3,9 +3,16 @@ package ru.spb.yakovlev.movieapp2020.data.db.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "movie_actor_x_refs")
+@Entity(
+    tableName = "movie_actor_x_refs",
+    indices = [
+        androidx.room.Index(value = ["movieId", "actorId"], unique = true),
+        androidx.room.Index(value = ["movieId"], unique = false),
+    ]
+)
 class MovieActorXRef(
     @PrimaryKey(autoGenerate = true) val uid: Int = 0,
     val movieId: Int = 0,
     val actorId: Int = 0,
+    val castId: Int = 0,
 )
