59f208c34040df7722ee0a6804667c737b41b9a2
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-03-02 00:50:40 +0300

    build.gradle.kts - versions update, WorkManager added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,21 +66,21 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.5.0-beta01")
     implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.2.1")
+    implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    implementation("androidx.recyclerview:recyclerview:1.2.0-beta01")
+    implementation("androidx.recyclerview:recyclerview:1.2.0-beta02")
 
     // Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
 
     // Activity KTX
-    implementation("androidx.activity:activity-ktx:1.2.0-rc01")
+    implementation("androidx.activity:activity-ktx:1.3.0-alpha03")
 
     // Fragment KTX
-    implementation("androidx.fragment:fragment-ktx:1.3.0-rc01")
+    implementation("androidx.fragment:fragment-ktx:1.3.0")
 
     // Lifecycle, ViewModel and LiveData
-    val lifecycleVersion = "2.3.0-rc01"
+    val lifecycleVersion = "2.3.0"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion")
@@ -91,10 +91,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
     // Coil
-    implementation("io.coil-kt:coil:1.1.0")
+    implementation("io.coil-kt:coil:1.1.1")
 
     // Retrofit + OkHttp
-    val okHttpVersion = "4.9.0"
+    val okHttpVersion = "5.0.0-alpha.2"
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:$okHttpVersion")
@@ -102,29 +102,36 @@ dependencies {
     //implementation ("com.squareup.okhttp3:mockwebserver:$okHttpVersion")
 
     // Paging library
-    implementation("androidx.paging:paging-runtime:3.0.0-alpha12")
+    implementation("androidx.paging:paging-runtime:3.0.0-beta01")
 
     // Dagger + Hilt
-    val daggerVersion = "2.30.1"
+    val daggerVersion = "2.33"
     implementation("com.google.dagger:dagger:$daggerVersion")
     kapt("com.google.dagger:dagger-compiler:$daggerVersion")
     implementation("com.google.dagger:hilt-android:${rootProject.extra["hiltVersion"]}")
-    kapt("com.google.dagger:hilt-android-compiler:${rootProject.extra["hiltVersion"]}")
-    val androidxHilt = "1.0.0-alpha02"
+    kapt("com.google.dagger:hilt-compiler:${rootProject.extra["hiltVersion"]}")
+    val androidxHilt = "1.0.0-alpha03"
     implementation("androidx.hilt:hilt-lifecycle-viewmodel:$androidxHilt")
     kapt("androidx.hilt:hilt-compiler:$androidxHilt")
+    implementation("androidx.hilt:hilt-work:$androidxHilt")
 
     //Room
-    val roomVersion = "2.3.0-alpha04"
+    val roomVersion = "2.3.0-beta02"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     kapt("androidx.room:room-compiler:$roomVersion")
+
+    // WorkManager
+    val workVersion = "2.5.0"
+    implementation("androidx.work:work-runtime-ktx:$workVersion")
+    implementation("androidx.work:work-gcm:$workVersion") // optional - GCMNetworkManager support
+    androidTestImplementation("androidx.work:work-testing:$workVersion") // optional - Test helpers
 
     //Log
     implementation("com.jakewharton.timber:timber:4.7.1")
 
     // Testing
-    testImplementation("junit:junit:4.13.1")
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
